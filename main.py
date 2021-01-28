edde07cc84c4d1a9e7b1ab52c6484136bf908108
Hassan <hassanajjar3@gmail.com>
2021-01-27 20:11:47 -0800

    Update main.py

--- main.py
+++ main.py
@@ -23,18 +23,19 @@ def play(board, player):
             turn = key_dict[player_input]
         except KeyError:
             # if the player enters something that isn't a number between 1-9, then the program will ask it again
-            player_input = input(f"{CRED}Please enter a valid move.{CEND} \n\nYour move {player}:\n").lower().strip()
-            turn = key_dict[player_input]
+            draw_board(board)
+            print(f"{CRED}Please enter a valid move.{CEND}")
+            continue
         row = turn[0]
         column = turn[1]
         if board[row][column] == '-':
             # if the space that the player wants to play in is empty, it is a valid move
+            board[row][column] = player[0]
             break
         else:
             # if the space is already taken, then it isn't a valid move and goes through the while loop
             draw_board(board)
-            print(f"{CRED}Please enter a valid move!{CEND}")
-    board[row][column] = player[0]
+            print(f"{CRED}That space is already taken.{CEND}")
     return board
 
 
@@ -65,11 +66,14 @@ def check_win(board, player):
     return False
 
 
-def check_draw(board):
+def check_draw(board, player):
     for row in board:
         if '-' in row:
+            # if there are any empty spaces in the board
             return False
-    print(f"{CVIOLET}Draw!")
+    if not check_win(board, player):
+        # if someone hadn't already won on the last move
+        print(f"{CVIOLET}Draw!")
     return True
 
 
@@ -81,18 +85,19 @@ def main():
         name_1, name_2 = "X", "O"
 
     board = new_board
-    draw_board(board)
     while True:
         # player 1's turn
         draw_board(board)
         draw_board(play(board, name_1))
-        if check_win(board, name_1) or check_draw(board):
+        if check_win(board, name_1) or check_draw(board, name_1):
+            # ends game
             break
 
         # player 2's turn
         draw_board(board)
         draw_board(play(board, name_2))
-        if check_win(board, name_2) or check_draw(board):
+        if check_win(board, name_2) or check_draw(board, name_2):
+            # ends game
             break
 
 
